191bb93c14c29b74bab6c96db5e329f1b8bd5496
Hoodr1ch666 <vdaniil466@gmail.com>
2022-12-15 17:10:54 +0300

    end project portfolio 1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,43 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-if (localStorage.getItem('darkMode') === 'dark') {
+//Проверка темы по системным найстройкам
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
    btnDarkMode.classList.add("dark-mode-btn--active");
    document.body.classList.add("dark");
 }
+//проверка темы в localStorange
+if (localStorage.getItem('darkMode') === 'dark') {
+   btnDarkMode.classList.add("dark-mode-btn--active");
+   document.body.classList.add("dark");
+} else if (localStorage.getItem('darkMode') === 'light') {
+   btnDarkMode.classList.remove("dark-mode-btn--active");
+   document.body.classList.remove("dark");
+}
+
+
+
+//
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener('change',(event) => {
+   const newColorScheme = event.matches ? "dark" : 'light';
+   // alert('change!!!')
+   if (newColorScheme === 'dark'){
+   btnDarkMode.classList.add("dark-mode-btn--active");
+   document.body.classList.add("dark");
+   localStorage.setItem('darkMode', 'dark');
+   }else{
+   btnDarkMode.classList.remove("dark-mode-btn--active");
+   document.body.classList.remove("dark");
+   localStorage.setItem('darkMode', 'light');
+   }
+})
+
 
    btnDarkMode.onclick = function () {
       btnDarkMode.classList.toggle("dark-mode-btn--active");
       const isDark = document.body.classList.toggle('dark');
       if (isDark) {
-         localStorage.setItem('DarkMode', 'dark')
-      }else {
-         localStorage.setItem('DarkMode', 'light')
+         localStorage.setItem('darkMode', 'dark');
+      } else {
+         localStorage.setItem('darkMode', 'light');
       }
 }
