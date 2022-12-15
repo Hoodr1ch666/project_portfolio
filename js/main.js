ba6941124de2f7a3587e7283a2ae7e3516de7e48
Hoodr1ch666 <vdaniil466@gmail.com>
2022-12-15 13:49:05 +0300

    111

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
+if (localStorage.getItem('darkMode') === 'dark') {
+   btnDarkMode.classList.add("dark-mode-btn--active");
+   document.body.classList.add("dark");
+}
+
    btnDarkMode.onclick = function () {
       btnDarkMode.classList.toggle("dark-mode-btn--active");
-document.body.classList.toggle('dark');
+      const isDark = document.body.classList.toggle('dark');
+      if (isDark) {
+         localStorage.setItem('DarkMode', 'dark')
+      }else {
+         localStorage.setItem('DarkMode', 'light')
+      }
 }
